4214286ecbb9341c213a5bd4f08e892d666b0240
geroo <geronimopylypchuk@gmail.com>
2022-04-02 12:45:03 -0300

    feat: video

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,20 +9,20 @@ import logo from "../img/space-station.png";
 export default function App() {
   return (
     <div>
-          <nav className="navbar navbar-light bg-light mb-5">
-              <a className="navbar-brand ps-3">
-                  <strong>
-                      <img src={logo} width="30" height="30" className="d-inline-block align-top mr-1" alt="" />
-                      Academia DeFi
-                  </strong>
-              </a>
-              <ul>
-                  <li><Link to="/">Home</Link></li>
-                  <li><Link to="/videos">Videos</Link></li>
-                  <li><Link to="/articulos">Articulos</Link></li>
-                  <li><Link to="/trabajos">Trabajos</Link></li>
-              </ul>
-          </nav>
+      <nav className="navbar navbar-light bg-light mb-5">
+          <a className="navbar-brand ps-3">
+              <strong>
+                  <img src={logo} width="30" height="30" className="d-inline-block align-top mr-1" alt="" />
+                  Academia DeFi
+              </strong>
+          </a>
+          <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/videos">Videos</Link></li>
+              <li><Link to="/articulos">Articulos</Link></li>
+              <li><Link to="/trabajos">Trabajos</Link></li>
+          </ul>
+      </nav>
       <Routes>
         <Route path='' element={<Home />} />
         <Route path='/videos' element={<Videos />} />
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,19 @@
+import YouTube from 'react-youtube';
+
 export default function Videos() {
     return (
         <div>
-        <h1>Videos</h1>
+            <h1>Videos</h1>
+            <YouTube
+                videoId={'2g811Eo7K8U'}
+                opts={{
+                    height: '390',
+                    width: '640',
+                    playerVars: { // https://developers.google.com/youtube/player_parameters
+                        autoplay: 0
+                    }
+                }}
+            />
         </div>
     );
 }
