112d67e0169e8f439e7a21adddb8c3aaebfb4a6f
geroo <geronimopylypchuk@gmail.com>
2022-04-02 16:01:11 -0300

    fix images

--- src/components/ArticuloSquare.jsx
+++ src/components/ArticuloSquare.jsx
@@ -2,14 +2,14 @@ import './styles/Articulos.css';
 
 export default function ArticuloSquare(props) {
     return (
-        <div id="articleSquare" class="card mb-3 pt-1 ps-1">
+        <div id="articleSquare" class="card mb-3 pt-1 ps-1 pb-1">
             <div class="row g-0">
                 <div class="col-md-4">
-                    <img src={props.image} class="img-fluid rounded-start" alt=""/>
+                    <a href={props.articleLink}><img width='400px' src={props.image} class="img-fluid rounded-start" alt=""/></a>
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <a id='title-article-id' href={`${props.articleLink}`} class="card-title">{props.title}</a>
+                        <a id='title-article-id' href={props.articleLink} class="card-title">{props.title}</a>
                         <p class="card-text">{props.description}</p>
                     </div>
                 </div>
--- src/components/Articulos.jsx
+++ src/components/Articulos.jsx
@@ -1,22 +1,27 @@
 import ArticuloSquare from './ArticuloSquare';
-import ico from '../img/ico.jpg';
 
 export default function Articulos() {
     return (
         <div>
-            <h1 className="position-absolute end-50 start-50 translate-middle pb-2">ARTÍCULOS</h1>
+            <h1 className="position-absolute end-50 start-50 translate-middle pb-2">ARTICULOS</h1>
             <div className="pt-5">
                 <ArticuloSquare
-                    image={ico}
-                    title="¿Qué es una ICO?"
-                    description="En este artículo aprenderás que es una ICO y cómo funciona."
-                    articleLink="https://startupsoasis.com/startups-espanolas-tokens-criptomonedas/"
+                    image='https://images.cointelegraph.com/images/1200_aHR0cHM6Ly9zMy5ldS1jZW50cmFsLTEuYW1hem9uYXdzLmNvbS9zMy5jb2ludGVsZWdyYXBoLmNvbS91cGxvYWRzLzIwMjAtMDkvMjY1MDg5ZDgtNzEwYi00MGZmLWE0NTYtMDBkODVkOGVhMjViLmpwZw==.jpg'
+                    title="¿Invertir en DeFi? Apuesta por la diversificación"
+                    description="Varios casos nos muestran que si bien hay increíbles oportunidades de ganancias en el espacio DeFi, también hay muchos riesgos que deben ser considerados."
+                    articleLink="https://es.cointelegraph.com/news/investing-in-defi-bet-on-diversification-not-short-term-gains"
                 />
                 <ArticuloSquare
-                    image={ico}
-                    title="¿Qué es una ICO?"
-                    description="En este artículo aprenderás que es una ICO y cómo funciona."
-                    articleLink="https://startupsoasis.com/startups-espanolas-tokens-criptomonedas/"
+                    image='https://www.cronista.com/files/image/389/389224/6173021bd9396_950_534!.webp?s=83fa9e4ad4c9f7064cc47730584c5e3c&d=1643750350&oe=jpg'
+                    title="DeFi, la inversión estrella que paga 15% en dólares: cómo entrar"
+                    description="Los protocolos de finanzas descentralizadas permiten a los argentinos depositar pesos y generar ganancias en dólares de hasta 15%. Cómo funcionan."
+                    articleLink="https://www.cronista.com/infotechnology/finanzas-digitales/defi-la-inversion-estrella-que-paga-15-en-dolares-como-entrar/"
+                />
+                <ArticuloSquare
+                    image='https://www.bbva.com/wp-content/uploads/2021/06/BBVA-DeFi-finanzas.descentralizadas-1024x629.jpg'
+                    title="El desembarco de las finanzas descentralizadas"
+                    description="Las finanzas descentralizadas son un conjunto de aplicaciones basadas en redes blockchain que, en principio, no necesitan de intermediarios para funcionar. El auge de estos productos financieros, que tienen características similares a los servicios tradicionales, podría cambiar en cierta medida el sector de las finanzas y trae consigo oportunidades y desafíos."
+                    articleLink="https://www.bbva.com/es/en-que-consisten-las-defi-o-finanzas-descentralizadas/"
                 />
             </div>
         </div>
