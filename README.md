464a287b585bb085bd685b95ceaccf54ed7bf66b
geroo <geronimopylypchuk@gmail.com>
2022-04-02 10:04:05 -0300

    feat: single page app

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## El Proyecto 📚
 Sitio web donde podras aprender finanzas relacionadas a blockchain (DeFi o Decentralized Finance)
 
-## Como Probarlo localmente
+## Cómo Probarlo localmente
 1. Cloná este repositorio.
 2. Instalá las dependencias tipeando: `npm install`
 3. Correlo en localhost:3000 con: `npm run dev`
--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import Home from './Home.jsx'
 import Videos from './Videos.jsx'
 import Articulos from './Articulos'
 import Trabajos from './Trabajos.jsx'
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
-      <h4>Academia Defi</h4>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/videos">Videos</a></li>
-          <li><a href="/articulos">Articulos</a></li>
-          <li><a href="/trabajos">Trabajos</a></li>
-        </ul>
-      </nav>
+      <header>
+        <h4>Academia Defi</h4>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/videos">Videos</Link></li>
+            <li><Link to="/articulos">Articulos</Link></li>
+            <li><Link to="/trabajos">Trabajos</Link></li>
+          </ul>
+        </nav>
+      </header>
       <Routes>
         <Route path='' element={<Home />} />
         <Route path='/videos' element={<Videos />} />
